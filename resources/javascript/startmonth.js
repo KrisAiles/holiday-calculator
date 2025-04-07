858e010b5e5499b0c3be53d60fcc522922bf9fe8
Kris <kris28@btinternet.com>
2025-04-07 21:39:38 +0100

    replace hard coded bank holidays with bank holiday api

--- resources/javascript/startmonth.js
+++ resources/javascript/startmonth.js
@@ -1,3 +1,68 @@
+const currentDate = new Date(Date.now());
+const currentMonth = currentDate.getMonth();
+const currentYear = currentDate.getFullYear();
+let yearPartOne;
+let yearPartTwo;
+
+if (currentMonth >= 0 && currentMonth <= 7) {
+    yearPartOne = currentYear - 1;
+    yearPartTwo = currentYear;
+} else {
+    yearPartOne = currentYear;
+    yearPartTwo = currentYear + 1;
+}
+
+const bankHolsArray = [];
+
+async function getBankHols() {
+    const url = 'https://www.gov.uk/bank-holidays.json';
+    const response = await fetch(url);
+    const json = await response.json();
+    const bankHols = json['england-and-wales'].events;
+    let sep = [];
+    let oct = [];
+    let nov = [];
+    let dec = [];
+    let jan = [];
+    let feb = [];
+    let mar = [];
+    let apr = [];
+    let may = [];
+    let jun = [];
+    let jul = [];
+    let aug = [];
+    for (let i = 0; i < bankHols.length; i++) {
+        if (bankHols[i].date.includes(`${yearPartOne}-09`)) {
+            sep.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartOne}-10`)) {
+            oct.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartOne}-11`)) {
+            nov.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartOne}-12`)) {
+            dec.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartTwo}-01`)) {
+            jan.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartTwo}-02`)) {
+            feb.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartTwo}-03`)) {
+            mar.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartTwo}-04`)) {
+            apr.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartTwo}-05`)) {
+            may.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartTwo}-06`)) {
+            jun.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartTwo}-07`)) {
+            jul.push(bankHols[i].date);
+        } else if (bankHols[i].date.includes(`${yearPartTwo}-08`)) {
+            aug.push(bankHols[i].date);
+        }
+    }
+    bankHolsArray.push(sep, oct, nov, dec, jan, feb, mar, apr, may, jun, jul, aug);
+}
+
+getBankHols();
+
 const proRataHoursForm = document.getElementById("pro-rata-hours-form");
 proRataHoursForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -104,33 +169,33 @@ proRataHoursForm.addEventListener("submit", (e) => {
             // pro rata holiday entitlement based on part time hours
             const ptHolEntProRata = (ftHolEntProRata / hpw) * ptHpw;
             // Number of bank holidays based on month of the financial year you start or your hours change
-            let ftBankHolEnt;
+            let ftBankHolEnt = 0;
             let mondayBankHol = 0;
             let tuesdayBankHol = 0;
             let wednesdayBankHol = 0;
             let thursdayBankHol = 0; 
             let fridayBankHol = 0;
-            if (startMonth >= 1 && startMonth <= 3) {
-                ftBankHolEnt = 1;
-                mondayBankHol = 1;
-            } else if (startMonth === 4) {
-                ftBankHolEnt = 3;
-                mondayBankHol = 3;
-            } else if (startMonth >= 5 && startMonth <= 7) {
-                ftBankHolEnt = 5;
-                mondayBankHol = 4;
-                fridayBankHol= 1;
-            } else if (startMonth === 8) {
-                ftBankHolEnt = 6;
-                mondayBankHol = 4;
-                wednesdayBankHol = 1;
-                fridayBankHol = 1;
-            } else {
-                ftBankHolEnt = 8;
-                mondayBankHol = 4;
-                wednesdayBankHol = 2;
-                thursdayBankHol = 1;
-                fridayBankHol = 1;
+            for (let i = bankHolsArray.length - startMonth; i < bankHolsArray.length; i++) {
+                ftBankHolEnt += bankHolsArray[i].length;
+                for (let j = 0; j < bankHolsArray[i].length; j++) {
+                    let day = (new Date(bankHolsArray[i][j])).getDay();
+                    switch(day) {
+                        case 1:
+                            mondayBankHol += 1;
+                            break;
+                        case 2: 
+                            tuesdayBankHol += 1;
+                            break;
+                        case 3:
+                            wednesdayBankHol += 1;
+                            break;
+                        case 4: 
+                            thursdayBankHol += 1;
+                            break;
+                        case 5: 
+                            fridayBankHol += 1;
+                    };
+                };
             };
             // number of bank holidays that fall on working days multiplied by the hours worked on these days then added to an array
             mondayBankHol *= mondayWorkingHours;
